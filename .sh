32c7187b39f6630b6d1dab5f8d72405f4f4233c8
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2026-01-08 01:19:28 -0800

    -

--- .sh
+++ .sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
+: > .gitattributes
+git lfs track "Themes/*"
 while read -r f
-	do yq --yaml-fix-merge-anchor-to-spec=true -o=json "$f" | jq "." > "${f%.yml}.json"
-done < <(find Data -name \*.yml)
+	do yq --yaml-fix-merge-anchor-to-spec=true -o=json "$f" \
+		| jq -c "del(._)" \
+		> "${f%.yml}.json"
+done < <(find Data -name "*.yml")
